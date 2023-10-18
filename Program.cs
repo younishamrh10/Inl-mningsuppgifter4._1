611b1e28a643275cf43d5d7995eb9ebf74861784
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-18 10:23:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inlämningsuppgifter4._1
+{
+    class Program
+    {
+        static void Main(string[] arga)
+        {
+            for (int i=10; i < 31; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 200; i > 179; i--)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int i = 1000; i < 1405; i+= 5)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
